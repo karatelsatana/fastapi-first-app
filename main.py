233f4183249ccca98d38260a3afb4f670d3bbf39
Karatel <alisherturarov77@gmail.com>
2026-07-02 03:03:44 +0500

    add sqlite db with create and read users

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import sqlite3
+
+def get_db():
+    conn = sqlite3.connect("users.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+conn = get_db()
+conn.execute("""
+CREATE TABLE IF NOT EXISTS users(
+             id INTEGER PRIMARY KEY AUTOINCREMENT,
+             name TEXT NOT NULL,
+             age INTEGER NOT NULL
+)
+""")
+conn.commit()
+conn.close()
+
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -23,4 +42,15 @@ class User(BaseModel):
 
 @app.post("/user")
 def create_user(user: User):
-    return {"сообщение": f"Пользователь {user.name}, {user.age} лет создан"}
+    conn = get_db()
+    conn.execute("INSERT INTO users (name, age) VALUES (?, ?)", (user.name, user.age))
+    conn.commit()
+    conn.close()
+    return {"сообщение": f"Пользователь {user.name}, {user.age} лет сохранен в базе"}
+
+@app.get("/users")
+def list_users():
+    conn = get_db()
+    rows = conn.execute("SELECT * FROM users").fetchall()
+    conn.close()
+    return [dict(row) for row in rows]
