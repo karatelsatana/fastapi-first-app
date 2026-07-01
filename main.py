d299c1465c605977d854b08fa7aa5b84b37bf530
Karatel <alisherturarov77@gmail.com>
2026-07-02 02:36:31 +0500

    add query params and POST endpoint with pydantic validation

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "работает"}
+
+@app.get("/hello/{name}")
+def greet(name: str):
+    return {"message": f"Привет, {name}"}
+
+@app.get("/user")
+def get_user(name: str, age: int):
+    return {"имя": name, "возраст": age}
+
+
+from pydantic import BaseModel
+
+class User(BaseModel):
+    name: str
+    age: int
+
+@app.post("/user")
+def create_user(user: User):
+    return {"сообщение": f"Пользователь {user.name}, {user.age} лет создан"}
